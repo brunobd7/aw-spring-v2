e3844778d4fd3a838b143938572ad6c78a906dd7
Bruno Dantas <brunoodantas7@gmail.com>
2023-04-27 21:37:39 -0300

    Post mapping of launches resource with eventListener and bean validation

--- src/main/java/com/dantas/algamoney/awspringv2/api/model/Launch.java
+++ src/main/java/com/dantas/algamoney/awspringv2/api/model/Launch.java
@@ -3,6 +3,7 @@ package com.dantas.algamoney.awspringv2.api.model;
 import com.dantas.algamoney.awspringv2.api.enums.LaunchType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,10 +22,12 @@ public class Launch {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     private String description;
 
     @Column(name = "due_date")
     @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull
     private LocalDate dueDate;
 
     @Column(name = "pay_date")
@@ -35,14 +38,17 @@ public class Launch {
     private BigDecimal amountValue;
 
     @Enumerated(EnumType.STRING)
+    @NotNull
     private LaunchType launchType;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @NotNull
     private Category category;
 
     @ManyToOne
     @JoinColumn(name = "person_id")
+    @NotNull
     private Person person;
 
 }
--- src/main/java/com/dantas/algamoney/awspringv2/api/resource/LaunchResource.java
+++ src/main/java/com/dantas/algamoney/awspringv2/api/resource/LaunchResource.java
@@ -1,14 +1,19 @@
 package com.dantas.algamoney.awspringv2.api.resource;
 
+import com.dantas.algamoney.awspringv2.api.event.ListenerResourceCreated;
+import com.dantas.algamoney.awspringv2.api.event.ResourceCreatedEvent;
 import com.dantas.algamoney.awspringv2.api.model.Launch;
 import com.dantas.algamoney.awspringv2.api.repository.LaunchRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/launches")
@@ -17,11 +22,31 @@ public class LaunchResource {
     @Autowired
     private LaunchRepository repository;
 
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
 
-    @GetMapping
+
+    @GetMapping("/")
     public ResponseEntity<List<Launch>> getAllLauches() {
         List<Launch> launchList = repository.findAll();
         return launchList.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(launchList);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Launch> getLaunchById(@PathVariable Long id){
+        Launch launchFounded = repository.findById(id).orElse(new Launch());
+
+        return Objects.isNull(launchFounded.getId())
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(launchFounded);
+
+    }
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createLaunch(@Valid @RequestBody Launch launch, HttpServletResponse response){
+        Launch createdLaunch = repository.save(launch);
+
+        eventPublisher.publishEvent(new ResourceCreatedEvent(createdLaunch, response,createdLaunch.getId()));
     }
 }
