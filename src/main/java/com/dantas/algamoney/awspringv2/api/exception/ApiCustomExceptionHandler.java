50ff74a7e88e63c3496b83a60cf144fe4d2e8fe0
Bruno Dantas <brunoodantas7@gmail.com>
2023-04-27 21:59:06 -0300

    New exception handle to data integrity and add new dependecy to access details of exceptions related a database operations

--- src/main/java/com/dantas/algamoney/awspringv2/api/exception/ApiCustomExceptionHandler.java
+++ src/main/java/com/dantas/algamoney/awspringv2/api/exception/ApiCustomExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.dantas.algamoney.awspringv2.api.exception;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,7 +16,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -42,6 +43,16 @@ public class ApiCustomExceptionHandler extends ResponseEntityExceptionHandler {
 
         List<CustomApiErrorMessage> errorsList = this.mapRequestErrors(ex.getBindingResult());
         return handleExceptionInternal(ex, errorsList,headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+
+        String userMessage = messageSource.getMessage("resource.notAllowedOperation",null, LocaleContextHolder.getLocale());
+        String exMessage = ExceptionUtils.getRootCauseMessage(ex); //USING APACHE COMMONS TO ACCESS DETAILS OF EXCEPTION RELATED A DATABASE
+
+        return handleExceptionInternal(ex, List.of(new CustomApiErrorMessage(userMessage, exMessage)),new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
     }
 
     @ExceptionHandler({EmptyResultDataAccessException.class})
