a034f495d157238fd1ce9bacee4d73c7188dbed0
Bruno Dantas <brunoodantas7@gmail.com>
2023-05-02 14:16:42 -0300

    Add spring security dependency and config a basic authentication in memory

---
+++ src/main/java/com/dantas/algamoney/awspringv2/api/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.dantas.algamoney.awspringv2.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+
+    // WebSecurityConfigurerAdapter was deprecated instead of override configure method of them, we should use two beans to make a security config
+    // one bean set HttpSecurity params and other to Authentication config
+
+
+    /** Using a SecurityFilterChain instead override configure method when extends WebSecurityConfigurerAdapter.
+     *  Then we can config ROUTES , MATCHER AND AUTHENTICATION TYPE.
+     *
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .authorizeHttpRequests()
+                .requestMatchers("/categories").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .build();
+    }
+
+    /** Defining inMemory user to do an AUTHENTICATION IN MEMORY*/
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManagerService(){
+
+        UserDetails userDetails = User.withUsername("admin").password("{noop}admin").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+
+}
