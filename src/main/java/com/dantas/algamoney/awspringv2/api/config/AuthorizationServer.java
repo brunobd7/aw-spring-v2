0bf835a7dde95de878ccb9136e06145f8ea47fbf
Bruno Dantas <brunoodantas7@gmail.com>
2023-05-03 23:58:16 -0300

    JWT Configuration in progress

--- src/main/java/com/dantas/algamoney/awspringv2/api/config/AuthorizationServer.java
+++ src/main/java/com/dantas/algamoney/awspringv2/api/config/AuthorizationServer.java
@@ -1,12 +1,33 @@
 package com.dantas.algamoney.awspringv2.api.config;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.JWTProcessor;
+import org.apache.logging.log4j.spi.Provider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -14,9 +35,13 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.UUID;
 
 @Configuration
@@ -28,21 +53,97 @@ public class AuthorizationServer {
     @Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
     private String clientSecret;
 
+    //TODO AFTER WE WILL CREATE A CLASS TO MANAGE ALL PROPERTY OF API
+    //	@Autowired
+    //	private AlgamoneyAPIProperty algamoneyAPIProperty;
+
     @Bean
-	public RegisteredClientRepository registeredClientRepository() {
-		RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-				.clientId(clientId)//ANGULAR APP
-				.clientSecret(clientSecret)//ANGULAR APP SECRET
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)//PASSWORD FLOW
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId(clientId)//ANGULAR APP
+                .clientSecret("{noop}".concat(clientSecret))//ANGULAR APP SECRET
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-				.scope("read")
-				.scope("write")
-				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .authorizationGrantType(AuthorizationGrantType.PASSWORD)//PASSWORD FLOW
+                .scope("read")
+                .scope("write")
+                .redirectUris(uri -> uri.add("http://localhost:4200/authorized"))//FRONTEND PAGE TO REDIRECT AFTER AUTHENTICATION SUCCESSFULL
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
                 .tokenSettings(TokenSettings.builder().accessTokenTimeToLive(Duration.ofMinutes(30)).build())
-				.build();
+                .build();
 
-		return new InMemoryRegisteredClientRepository(registeredClient);
-	}
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
 
+    /**
+     *  HOW EXIST N SECURITY FILTERS WE MUST DEFINE HIGHEST PRIORITY OF OUR OAUTH SERVER FILTER
+     *  Annotation @Order define priority of this filter.
+     */
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain oauthServerFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+        return httpSecurity.formLogin(Customizer.withDefaults()).build();// NOW LOGIN FORM IS HANDLE BY OUR AUTHORIZATION SERVER
+    }
+
+
+    /**
+     * Customizing JWT , we should pass user and pass data. Authorities and roles too
+     */
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtBuildCustomizer() {
+        return context -> {
+            context.getClaims().claim("nome", "userName");
+            context.getClaims().claim("authorities", new HashSet<String>());// ROLES / AUTHORITIES
+        };
+    }
+
+
+    /**
+     * BEAN GENERATE KEYS TO DO A SINGNATURE OF TOKEN
+     * DISCLAIMER -> THE DEPRECATED WAY WIHT ALGORITM ENCODER H256 DON'T WORK, MINIMUM SECURITY KEY ARE RSA KEYS
+     */
+    @Bean
+    public JWKSet jwkSet() throws JOSEException {
+        RSAKey rsa = new RSAKeyGenerator(2048)
+                .keyUse(KeyUse.SIGNATURE)
+                .keyID(UUID.randomUUID().toString())
+                .generate();
+
+        return new JWKSet(rsa);
+    }
+
+    /**
+     * BEAN TO READ JWKSET BEAN AND COMUNICATE TO SPRING WICH SIGNATURE KEY WE ARE USING
+     */
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(JWKSet jwkSet) {
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    /**
+     * ENCODER FOR JWT TOKEN
+     */
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    /**
+     * DECODER FOR JWT TOKEN
+     */
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    /**
+     * BEAN TO SET ISSUER OF TOKEN , OTHERWISE WHO ARE CREATOR/EMITTER OF THIS TOKEN
+     */
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder()
+                .issuer("http://localhost:8080/authServerUrl")
+                .build();
+    }
 
 }
--- src/main/java/com/dantas/algamoney/awspringv2/api/config/ResourceServerConfig.java
+++ src/main/java/com/dantas/algamoney/awspringv2/api/config/ResourceServerConfig.java
@@ -3,13 +3,23 @@ package com.dantas.algamoney.awspringv2.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -24,14 +34,38 @@ public class ResourceServerConfig {
                 .requestMatchers("/categories/").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//REMOVE TO USE AuthorizationGrantType CLIENT_CREDENTIALS
                 .and()
                 .csrf().disable()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer()
+                .jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return httpSecurity.build();
     }
 
+     private JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			List<String> authorities = jwt.getClaimAsStringList("authorities");
+
+			if (authorities == null) {
+				authorities = Collections.emptyList();
+			}
+
+			JwtGrantedAuthoritiesConverter scopesAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+			Collection<GrantedAuthority> grantedAuthorities = scopesAuthoritiesConverter.convert(jwt);
+
+			grantedAuthorities.addAll(authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList()));
+
+			return grantedAuthorities;
+		});
+
+		return jwtAuthenticationConverter;
+	}
 
     /**
      * Defining inMemory user to do an AUTHENTICATION IN MEMORY
